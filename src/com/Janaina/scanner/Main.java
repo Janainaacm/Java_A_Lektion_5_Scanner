745906610730bf01df46e33021d741c931b6e2ec
janainaacm <janaina@Janainas-MacBook-Air.local>
2023-09-19 13:14:35 +0200

    Scanner practice

--- src/com/Janaina/scanner/Main.java
+++ src/com/Janaina/scanner/Main.java
@@ -1,10 +1,47 @@
 package com.Janaina.scanner;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        boolean isPlaying = true;
+
+        do {
+
+            int result = scannerNumber();
+
+            if (result > 1000){
+                isPlaying = false;
+            }
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping");
+            }
 
 
+        } while (isPlaying);
+
+    }
+
+    public static int scannerNumber(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+        return sc.nextInt();
+
+    }
+
+
+    public static String scannerText(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        String input = sc.nextLine();
+
+        return input;
 
     }
 
