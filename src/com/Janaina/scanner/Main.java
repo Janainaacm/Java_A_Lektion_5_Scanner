8214402c620106e1cd5ba4fbf4f4fe78eb720ac8
janainaacm <janaina@Janainas-MacBook-Air.local>
2023-09-19 13:41:54 +0200

    Student scanner implementation

--- src/com/Janaina/scanner/Main.java
+++ src/com/Janaina/scanner/Main.java
@@ -4,16 +4,24 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        //Objects
         Scanner sc = new Scanner(System.in);
+        Student benny = new Student();
 
+
+        //Game
         do {
 
-            System.out.println("Input number");
+            System.out.println("Input bennys age");
             int result = sc.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("Benny's age is: " + benny.age);
 
         } while (true);
 
---
+++ src/com/Janaina/scanner/Student.java
@@ -0,0 +1,13 @@
+package com.Janaina.scanner;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public boolean isTired;
+
+
+
+
+
+}
